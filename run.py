11ae92ce497843bcd3b38a3cec9ba43b9afd8492
Czechbol <adamludes@gmail.com>
2020-11-17 01:30:43 +0100

    fixed BSGS result

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from functions import prime
 
 
 if __name__ == "__main__":
-    p: int = prime.getPrime(16)  # publicly known, generates a random prime number of bit size
+    p: int = prime.getPrime(22)  # publicly known, generates a random prime number of bit size
     g: int = random.randint(1, p - 1)  # publicly known
     a: int = random.randint(1, p)  # only Alice knows this
     b: int = random.randint(1, p)  # only Bob knows this
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     bobComputes = pow(aliceSends, b, p)
 
     print("P is ", p, ", G is ", g)
-    print("A is ", a, ", B is ", b)
+    print("a is ", a, ", b is ", b)
     print()
     print("Alice sends: " + str(aliceSends) + ", Bob sends: " + str(bobSends))
     print("Alice computes: " + str(aliceComputes) + ", Bob computes: " + str(bobComputes))
@@ -36,10 +36,17 @@ if __name__ == "__main__":
     start = timer()
 
     if 1 == a:
-        cracked_CPU = cpu_brute_mt.calculate_key(p, g, aliceSends, bobSends)
+        cracked_key = cpu_brute_mt.calculate_key(p, g, aliceSends, bobSends)
     elif 2 == a:
-        cracked_CPU = babystep_giantstep.calculate_key(p, g, aliceSends)
+        temp = babystep_giantstep.calculate_key(p, g, aliceSends)
+        log = pow(g, temp, p)
+        if log == aliceSends:
+            cracked_key = pow(bobSends, temp, p)
+            print("Found A: ", temp)
+        if log == bobSends:
+            cracked_key = pow(aliceSends, temp, p)
+            print("Found B: ", temp)
 
-    print("Cracked key ", cracked_CPU)
+    print("Cracked key ", cracked_key)
     print()
     print("Cracking took took", timer() - start, "seconds to run")
